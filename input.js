5bd71aae9f95b7b1eef1b2499f527b36eee7ea4f
aliy0012 <alikamal415@yahoo.com>
2021-11-03 21:14:21 +0000

    movements added

--- input.js
+++ input.js
@@ -1,20 +1,36 @@
+const connect = require("./client");
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
   stdin.resume();
-  stdin.on("data", handleUserInput);
   return stdin;
 };
+
+
 
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
-  }
-  else {
-    console.log(data);
-  }
+  } else if(data === 'w') {
+    connection.write("Move: up");
+  } else if(data === 'a') {
+    connection.write("Move: left");
+  } else if(data === 's') {
+    connection.write("Move: down");
+  } else if(data === 'd') {
+    connection.write("Move: right");
+  } 
+
 };
+setupInput();
 
 module.exports = setupInput;
